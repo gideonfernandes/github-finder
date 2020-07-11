e46c7cfd249e4f3d5c4d69b93d41e5b65669fc92
gideonfernandes <gideon.de.fernandes@gmail.com>
2020-07-11 19:59:47 -0300

    Refactor Navbar and UserItem components to functional component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-	render() {
-		const { id, login, avatar_url, html_url } = this.props.user;
+const UserItem = ({ user: { id, login, avatar_url, html_url } }) => {
+	return (
+		<div className='card text-center'>
+			<img
+				src={avatar_url}
+				alt={login}
+				className='round-img'
+				style={{ width: '60px' }}
+			/>
 
-		return (
-			<div className='card text-center'>
-				<img
-					src={avatar_url}
-					alt={login}
-					className='round-image'
-					style={{ width: '60px' }}
-				/>
+			<h3>{login}</h3>
 
-				<h3>{login}</h3>
+			<a
+				href={html_url}
+				className='btn btn-sm btn-dark my-1'>Mais Informações
+			</a>
+		</div>
+	)
+};
 
-				<a
-					href={html_url}
-					className='btn btn-sm btn-dark my-1'>Mais Informações
-				</a>
-			</div>
-		)
-	};
+UserItem.propTypes = {
+	user: PropTypes.object.isRequired
 };
 
 export default UserItem;
