68876453ef8f68bcf3395e8acf18647791a2ee87
gideonfernandes <gideon.de.fernandes@gmail.com>
2020-07-13 22:39:32 -0300

    Add User component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Users from './components/users/Users';
+import User from './components/users/User';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import axios from 'axios';
@@ -11,6 +12,7 @@ import './App.css';
 class App extends Component {
 	state = {
 		users: [],
+    user: {},
 		loading: false,
     alert: null
 	};
@@ -37,6 +39,16 @@ class App extends Component {
     this.setState({ users: response.data.items, loading: false })
   };
 
+  getSingleUser = async username => {
+    this.setState({ loading: true })
+
+    const response = await axios.get(`https://api.github.com/users/${username}?client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+    this.setState({ user: response.data, loading: false })
+  };
+
   clearUsers = () => this.setState({ users: [], loading: false });
 
   setAlert = (message, type) => {
@@ -45,7 +57,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading } = this.state;
+    const { users, user, loading } = this.state;
     
     return (
       <Router>
@@ -69,6 +81,14 @@ class App extends Component {
                 </Fragment>
               )} />
               <Route exact path="/about" component={About} />
+              <Route exact path="/user/:username" render={props => (
+                <User
+                  { ...props }
+                  getUser={this.getSingleUser}
+                  user={user}
+                  loading={loading}
+                />
+              )} />
             </Switch>
           </main>
         </div>
---
+++ src/components/users/User.js
@@ -0,0 +1,107 @@
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+class User extends Component {
+	componentDidMount() {
+		this.props.getUser(this.props.match.params.username)
+	};
+
+	static propTypes = {
+		loading: PropTypes.bool.isRequired,
+		user: PropTypes.object.isRequired,
+		getUser: PropTypes.func.isRequired
+	};
+
+	render() {
+		const {
+			name,
+			avatar_url,
+			company,
+			location,
+			bio,
+			blog,
+			login,
+			html_url,
+			followers,
+			following,
+			public_repos,
+			public_gists,
+			hireable
+		} = this.props.user
+
+		const { loading } = this.props
+
+		if (loading) return <Spinner />
+
+		return (
+			<Fragment>
+				<Link to="/" className="btn btn-light">
+					<i className="fas fa-arrow-left"></i> Voltar para a página de busca
+				</Link>
+				Contratável:{' '}
+				{hireable ? (
+					<i className="fas fa-check text-success"></i>
+				) : (
+					<i className="fas fa-times-circle text-danger"></i>
+				)}
+
+				<section className="card grid-2">
+					<div className="all-center">
+						<img
+							src={avatar_url}
+							alt={name}
+							className="round-img"
+							style={{ width: '150px' }}
+						/>
+						<h1>{name}</h1>
+						<p>Localização: {location}</p>
+					</div>
+					<div>
+						{bio && (
+							<Fragment>
+								<h3>Resumo Descritivo</h3>
+								<p>{bio}</p>
+							</Fragment>
+						)}
+
+						<a href={html_url} className="btn btn-dark my-1">Visitar Perfil no Github</a>
+
+						<ul>
+							<li>
+								{login && (
+									<Fragment>
+										<strong>Nome de Usuário:</strong> {login}
+									</Fragment>
+								)}
+							</li>
+							<li>
+								{company && (
+									<Fragment>
+										<strong>Empresa:</strong> {company}
+									</Fragment>
+								)}
+							</li>
+							<li>
+								{blog && (
+									<Fragment>
+										<strong>Website:</strong> {blog}
+									</Fragment>
+								)}
+							</li>
+						</ul>
+					</div>
+				</section>
+				<section className="card text-center">
+					<span className="badge badge-primary">Seguidores: {followers}</span>
+					<span className="badge badge-success">Seguindo: {following}</span>
+					<span className="badge badge-light">Repositórios Publicos: {public_repos}</span>
+					<span className="badge badge-dark">Gists Públicos: {public_gists}</span>
+				</section>
+			</Fragment>
+		)
+	};
+};
+
+export default User;
